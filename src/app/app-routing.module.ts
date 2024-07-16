6c6b8ba44969778a8da12dd057457e17cfdadd05
Muhammadtawil <muhammad.tawil@hotmail.com>
2024-07-16 06:31:10 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+const routes: Routes = [
+  { path: '',  redirectTo: 'home',pathMatch:'full'},
+
+  {
+    path: 'home',
+    component: HomeComponent,
+
+  },
+  {
+    path: 'category',
+    component: CategoryComponent,
+
+  },
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
